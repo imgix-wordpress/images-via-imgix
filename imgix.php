0acfd04f874557b2d247f3a216faee6f09f3c7d6
Luke Watts <luke@thisis.la>
2016-10-24 22:06:30 +0100

    Updates markup to WordPress coding standards

--- imgix.php
+++ imgix.php
@@ -1,30 +1,39 @@
 <?php
-/*
-Plugin Name: imgix
-Description: A WordPress plugin to automatically use your existing (and future) WordPress images via <a href="http://www.imgix.com" target="_blank">imgix</a> for smaller, faster, and better looking images. <a href="https://github.com/wladston/imgix-wordpress" target="_blank">Learn more</a>.
-Author: imgix
-Author URI: http://www.imgix.com
-Version: 1.1.0
-*/
+/**
+ * imgix
+ *
+ * @package imgix
+ * @author wladston
+ * @license BSD-2
+ *
+ * @wordpress-plugin
+ * Plugin Name: imgix
+ * PLugin URI:  https://github.com/wladston/imgix-wordpress
+ * Description: A WordPress plugin to automatically use your existing (and future) WordPress images via <a href="http://www.imgix.com" target="_blank">imgix</a> for smaller, faster, and better looking images. <a href="https://github.com/wladston/imgix-wordpress" target="_blank">Learn more</a>.
+ * Version:     1.1.0
+ * Author:      wladston
+ * Author URI:  http://github.com/wladston
+ */
 
 // Variables
-$imgix_options = get_option('imgix_settings');
+$imgix_options = get_option( 'imgix_settings' );
 
-include('includes/do-functions.php');
-include('includes/options_page.php');
+include( 'includes/do-functions.php' );
+include( 'includes/options_page.php' );
 
 // Settings
-function imgix_plugin_admin_action_links($links, $file) {
+function imgix_plugin_admin_action_links( $links, $file ) {
 	static $my_plugin;
-	if (!$my_plugin) {
-		$my_plugin = plugin_basename(__FILE__);
+
+	if ( ! $my_plugin ) {
+		$my_plugin = plugin_basename( __FILE__ );
 	}
-	if ($file == $my_plugin) {
+
+	if ( $file === $my_plugin ) {
 		$settings_link = '<a href="options-general.php?page=imgix-options">Settings</a>';
-		array_unshift($links, $settings_link);
+		array_unshift( $links, $settings_link );
 	}
 	return $links;
 }
 
-add_filter('plugin_action_links', 'imgix_plugin_admin_action_links', 10, 2);
-?>
+add_filter( 'plugin_action_links', 'imgix_plugin_admin_action_links', 10, 2 );
