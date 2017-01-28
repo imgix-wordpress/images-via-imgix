0fa04cef38c51b7e52f86079481873565be67a63
Luke Watts <luke@thisis.la>
2017-01-28 19:00:25 +0000

    Merges changes from PR #14

--- includes/compability.php
+++ includes/compability.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Adds compability for old WordPress versions
+ * Adds compability for missing methods
  *
  * @package imgix
  */
@@ -51,5 +51,28 @@ if ( ! function_exists( 'wp_parse_url' ) ) {
 		}
 
 		return $parts;
+	}
+}
+
+if ( ! function_exists( 'http_build_url' ) ) {
+	/**
+	 * This is a simplified version of http_build_url if pecl_http is missing
+	 *
+	 * @param array $parsed_url
+	 *
+	 * @return string
+	 */
+	function http_build_url( $parsed_url ) {
+		$scheme   = isset( $parsed_url['scheme'] ) ? $parsed_url['scheme'] . '://' : '';
+		$host     = isset( $parsed_url['host'] ) ? $parsed_url['host'] : '';
+		$port     = isset( $parsed_url['port'] ) ? ':' . $parsed_url['port'] : '';
+		$user     = isset( $parsed_url['user'] ) ? $parsed_url['user'] : '';
+		$pass     = isset( $parsed_url['pass'] ) ? ':' . $parsed_url['pass'] : '';
+		$pass     = ( $user || $pass ) ? "$pass@" : '';
+		$path     = isset( $parsed_url['path'] ) ? $parsed_url['path'] : '';
+		$query    = isset( $parsed_url['query'] ) ? '?' . $parsed_url['query'] : '';
+		$fragment = isset( $parsed_url['fragment'] ) ? '#' . $parsed_url['fragment'] : '';
+
+		return "$scheme$user$pass$host$port$path$query$fragment";
 	}
 }
--- includes/do-functions.php
+++ includes/do-functions.php
@@ -10,194 +10,76 @@
  * source using the "dpr=2" modifier.
  *
  * @param $content
+ *
  * @return string Content with retina-enriched image tags.
  */
 function imgix_add_retina( $content ) {
 	$pattern = '/<img((?![^>]+srcset )([^>]*)';
 	$pattern .= 'src=[\'"]([^\'"]*imgix.net[^\'"]*\?[^\'"]*w=[^\'"]*)[\'"]([^>]*)*?)>/i';
-	$repl = '<img$2src="$3" srcset="${3}, ${3}&amp;dpr=2 2x, ${3}&amp;dpr=3 3x,"$4>';
+	$repl    = '<img$2src="$3" srcset="${3}, ${3}&amp;dpr=2 2x, ${3}&amp;dpr=3 3x,"$4>';
 	$content = preg_replace( $pattern, $repl, $content );
 
 	return preg_replace( $pattern, $repl, $content );
 }
 
 /**
- * Extract all img tags from a given $content block into an array.
+ * Returns a array of global parameters to be applied in all images,
+ * according to plugin's settings.
  *
- * @return array An array of matching arrays with two keys: 'url' and 'params'
+ * @return array Global parameters to be appened at the end of each img URL.
  */
-function imgix_extract_imgs( $content ) {
-	preg_match_all( '/src=["\']http.+\/([^\s]+?)["\']/', $content, $matches );
-	$results = array();
-
-	if ( $matches ) {
-		foreach ( $matches[1] as $url ) {
-			array_push( $results, $url );
-		}
-	}
-
-	return $results;
-}
-
-/**
- * Searches "$content" for all occurences of "$url" and add the given
- * querystring parameters to the URL, preserving existing querystring
- * parameters.
- *
- * @return string Content with matching URLs having the new querystrings.
- */
-function imgix_apply_parameters_to_url( $url, $params, $content ) {
-	$parts = explode( '?', $url . '?' );
-
-	list( $base_url, $base_params ) = array( $parts[0], $parts[1] );
-
-	$new_url = $old_url = $base_url;
-	$new_url .= '?' . $params;
-	$new_url .= $base_params ? '&amp;' . $base_params : '';
-	$old_url .= $base_params ? '?' . $base_params : '';
-
-	return str_replace( $old_url, $new_url, $content );
-}
-
-/**
- * Returns a string of global parameters to be applied in all images,
- * acording to plugin's settings.
- *
- * @return string Global parameters to be appened at the end of each img URL.
- */
-function imgix_get_global_params_string() {
+function imgix_get_global_params() {
 	global $imgix_options;
-	$params = array();
-	// For now, only "auto" is supported.
-	$auto = array();
 
-	if ( isset( $imgix_options['auto_format'] ) && $imgix_options['auto_format'] ) {
+	$params = [];
+	// For now, only "auto" is supported.
+
+	$auto = [];
+	if ( ! empty( $imgix_options['auto_format'] ) ) {
 		array_push( $auto, 'format' );
 	}
 
-	if ( isset( $imgix_options['auto_enhance'] ) && $imgix_options['auto_enhance'] ) {
+	if ( ! empty( $imgix_options['auto_enhance'] ) ) {
 		array_push( $auto, 'enhance' );
 	}
 
 	if ( ! empty( $auto ) ) {
-		array_push( $params, 'auto=' . implode( '%2C', $auto ) );
+		$params['auto'] = implode( ',', $auto );
 	}
 
-	return implode( '&amp;', $params );
+	return $params;
 }
 
 /**
- * Sanitize a given URL to make sure it has a scheme (or alternatively, '//' ),
- * host, path, and ends with a '/'.
+ * Convert sizes in filename to parameters and returns origina filename without sizes.
+ * If no size is found the original filename is returned.
  *
- * @return string A sanitized URL.
+ * @param string $filename
+ *
+ * @return array with filename and size arguments.
  */
-function imgix_ensure_valid_url( $url ) {
-	$slash = strpos( $url, '//' ) == 0 ? '//' : '';
+function convert_filename_to_size_args( $filename ) {
+	$arguments = [];
 
-	if ( $slash ) {
-		$url = substr( $url, 2 );
-	}
+	$filename = preg_replace_callback( '/-(?<width>\d+)x(?<height>\d+)(?<extension>\.\w{3,4}$)/', function ( $match ) use ( &$arguments ) {
+		$arguments = [
+			'w' => $match['width'],
+			'h' => $match['height']
+		];
 
-	$urlp = wp_parse_url( $url );
-	$pref = array_key_exists( 'scheme', $urlp ) ? $urlp['scheme'] . '://' : $slash;
+		return $match['extension'];
+	}, $filename );
 
-	if ( ! $slash && strpos( $pref, 'http' ) !== 0 ) {
-		$pref = 'http://';
-	}
-
-	$result = ! empty( $urlp['host'] ) ? $pref . $urlp['host'] : false;
-
-	if ( false !== $result ) {
-		return trailingslashit( $result );
-	}
-
-	return null;
+	return [ $filename, $arguments ];
 }
 
 /**
- * Parse through img element src attributes to extract height and width parameters
- * based on the structure of the src URL's path.
+ * Modify image urls for attachments to use imgix host.
  *
- * For example, if we are giving an img src ending in "-400x300.png",
- * we return an array structured like so:
+ * @param string $url
  *
- *    array(array(
- *      'raw' => '-400x300.png',
- *      'w' => '400',
- *      'h' => '300',
- *      'type' => 'png',
- *      'extra' => ''
- *    ))
- *
- * @return array An array of arrays that has extracted the URL's inferred w',
- * 'h', and 'type'
+ * @return string
  */
-function imgix_extract_img_details( $content ) {
-	preg_match_all( '/-([0-9]+)x([0-9]+)\.([^"\']+)/', $content, $matches );
-
-	$lookup = array( 'raw', 'w', 'h', 'type' );
-	$data = array();
-
-	if ( ! is_array($matches) ) {
-		return $data;
-	}
-
-	foreach ( $matches as $k => $v ) {
-		foreach ( $v as $index => $value ) {
-			if ( ! array_key_exists( $index, $data ) ) {
-				$data[ $index ] = array();
-			}
-
-			$key = $lookup[ $k ];
-
-			if ( $key === 'type' ) {
-				if ( strpos( $value, '?' ) !== false ) {
-					$parts = explode( '?', $value );
-					$data[ $index ]['type'] = $parts[0];
-					$data[ $index ]['extra'] = $parts[1];
-				} else {
-					$data[ $index ]['type'] = $value;
-					$data[ $index ]['extra'] = '';
-				}
-			} else {
-				$data[ $index ][ $key ] = $value;
-			}
-		}
-	}
-
-	return $data;
-}
-
-/**
- * Finds references to the wordpress site URL in the given string,
- * (optionally prefixed by "src"), and changes them to the imgix URL.
- *
- * @return array An array countaining the final string, and a boolean value
- * indicating if it's different from the given input string.
- */
-function imgix_replace_host( $str, $require_prefix = false ) {
-	global $imgix_options;
-
-	if ( ! isset( $imgix_options['cdn_link'] ) || ! $imgix_options['cdn_link'] ) {
-		return array( $str, false );
-	}
-
-	$new_host = imgix_ensure_valid_url( $imgix_options['cdn_link'] );
-	if ( ! $new_host ) {
-		return array( $str, false );
-	}
-
-	// As soon as srcset is supported…
-	// $prefix = $require_prefix? 'srcs?e?t?=[\'"]|,[\S+\n\r\s]*': '';
-	$prefix = $require_prefix ? 'src=[\'"]' : '';
-	$src = '(' . preg_quote( home_url( '/' ), '/' ) . '|\/\/)';
-	$patt = '/(' . $prefix . ' )' . $src . '/i';
-	$str = preg_replace( $patt, '$1' . $new_host, $str, -1, $count );
-
-	return array( $str, (boolean) $count );
-}
-
 function imgix_file_url( $url ) {
 
 	global $imgix_options;
@@ -208,54 +90,82 @@ function imgix_file_url( $url ) {
 
 	$file = pathinfo( $url );
 
-	if ( in_array( $file['extension'], array( 'jpg', 'gif', 'png', 'jpeg' ) ) ) {
-		return str_replace( get_bloginfo( 'wpurl' ), $imgix_options['cdn_link'], $url );
+	if ( in_array( $file['extension'], [ 'jpg', 'gif', 'png', 'jpeg' ] ) ) {
+		return str_replace( trailingslashit( home_url( '/' ) ), trailingslashit( $imgix_options['cdn_link'] ), $url );
 	}
 
 	return $url;
 }
+
 add_filter( 'wp_get_attachment_url', 'imgix_file_url' );
 add_filter( 'imgix/add-image-url', 'imgix_file_url' );
 
 /**
+ * Modify image urls in srcset to use imgix host.
  *
- * @param array         $sources
- * @param array         $size_array
- * @param string        $image_src
- * @param array         $image_meta
- * @param $attachment_id
+ * @param array $sources
  *
  * @return array $sources
  */
-function imgix_cdn_srcset( $sources, $size_array, $image_src, $image_meta, $attachment_id ) {
+function imgix_cdn_srcset( $sources ) {
 	foreach ( $sources as $source ) {
-
 		$sources[ $source['value'] ]['url'] = apply_filters( 'imgix/add-image-url', $sources[ $source['value'] ]['url'] );
-
 	}
 
 	return $sources;
 }
-add_filter( 'wp_calculate_image_srcset', 'imgix_cdn_srcset', 10, 5 );
 
+add_filter( 'wp_calculate_image_srcset', 'imgix_cdn_srcset', 10 );
+
+/**
+ * Modify image urls in content to use imgix host.
+ *
+ * @param $content
+ *
+ * @return string
+ */
 function imgix_replace_non_wp_images( $content ) {
-	list( $content, $match ) = imgix_replace_host( $content, true );
+	global $imgix_options;
 
-	if ( $match ) {
-		// Apply image-tag-encoded params for every image in $content.
-		foreach ( imgix_extract_img_details( $content ) as $img ) {
-			$to_replace = $img['raw'];
-			$extra_params = $img['extra'] ? '&amp;' . $img['extra'] : '';
-			$new_url = '.' . $img['type'] . '?h=' . $img['h'] . '&amp;w=' . $img['w'] . $extra_params;
-			$content = str_replace( $to_replace, $new_url, $content );
-		}
+	if ( ! empty( $imgix_options['cdn_link'] ) ) {
 
-		// Apply global parameters.
-		$g_params = imgix_get_global_params_string();
-		foreach ( imgix_extract_imgs( $content ) as $img_url ) {
-			$content = imgix_apply_parameters_to_url( $img_url, $g_params, $content );
-		}
+		$content = preg_replace_callback( '/(?<=\shref="|\ssrc="|\shref=\'|\ssrc=\').*(?=\'|")/', function ( $match ) use ( $imgix_options ) {
+			$url = $match[0];
+
+			$pathinfo = pathinfo( $url );
+
+			if ( in_array( $pathinfo['extension'], [ 'jpg', 'gif', 'png', 'jpeg' ], true ) ) {
+				$file_url = parse_url( $url );
+				if ( $file_url['host'] === parse_url( home_url( '/' ), PHP_URL_HOST ) ) {
+					$cdn = parse_url( $imgix_options['cdn_link'] );
+					foreach ( [ 'scheme', 'host', 'port' ] as $url_part ) {
+						if ( isset( $cdn[ $url_part ] ) ) {
+							$file_url[ $url_part ] = $cdn[ $url_part ];
+						} else {
+							unset( $file_url[ $url_part ] );
+						}
+					}
+
+					list( $filename, $arguments ) = convert_filename_to_size_args( $pathinfo['basename'] );
+
+					$arguments = array_merge( $arguments, imgix_get_global_params() );
+
+					$file_url['path'] = trailingslashit( dirname( $file_url['path'] ) ) . $filename;
+
+					if ( ! empty( $arguments ) ) {
+						$file_url['query'] = empty( $file_url['query'] ) ? build_query( $arguments ) : $file_url['query'] . '&' . build_query( $arguments );
+					}
+
+					$url = http_build_url( $file_url );
+				}
+			}
+
+
+			return esc_url_raw( $url );
+		}, $content );
+
 	}
+
 	return $content;
 }
 
